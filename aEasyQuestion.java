4e6a90c8c7ede95c91d09a6491ecd75648fa5bf5
Ehsan Rezvani <92954023+Ehsan-Rezvani@users.noreply.github.com>
2021-10-22 02:32:22 +0430

    Add files via upload

---
+++ aEasyQuestion.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class aEasyQuestion {
+    public static void main(String[] args) {
+        int a = new Scanner(System.in).nextInt();
+        while(a -- > 0) System.out.println("man khoshghlab hastam");
+    }
+}
