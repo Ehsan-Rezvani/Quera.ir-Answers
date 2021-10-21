29994be897d6b080199591ed53206827cb32b7e5
Ehsan Rezvani <92954023+Ehsan-Rezvani@users.noreply.github.com>
2021-10-22 02:22:54 +0430

    Add files via upload

---
+++ yellowQ.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class yellowQ {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = Integer.parseInt(in.nextLine());
+        System.out.print("W");
+        for (int i = 0; i < a; i++) System.out.print("o");
+        System.out.print("w!");
+    }
+}
